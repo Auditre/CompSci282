8a1bc30696a5472f8f3460ac194a2f6406dba492
Paul Gotcher <33100956+Auditre@users.noreply.github.com>
2018-09-26 23:04:31 -0700

    Functional FixedRecord class
    Need to generate new file for FixedRecords with spaces included

--- Project0/src/readWrite/FixedRecord.java
+++ Project0/src/readWrite/FixedRecord.java
@@ -2,7 +2,8 @@ package readWrite;
 
 public class FixedRecord {
 
-	private char[] firstname = new char[10];
+	
+	public char[] firstname = new char[10];
 	private char[] lastname = new char[15];
 	private char[] email = new char[25];
 	private char[] idnumber = new char[10];
@@ -11,5 +12,52 @@ public class FixedRecord {
 	private char[] fill;
 	
 	
+	public char[] getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(char[] firstname) {
+		this.firstname = firstname;
+	}
+	public char[] getLastname() {
+		return lastname;
+	}
+	public void setLastname(char[] lastname) {
+		this.lastname = lastname;
+	}
+	public char[] getEmail() {
+		return email;
+	}
+	public void setEmail(char[] email) {
+		this.email = email;
+	}
+	public char[] getIdnumber() {
+		return idnumber;
+	}
+	public void setIdnumber(char[] idnumber) {
+		this.idnumber = idnumber;
+	}
+	public char[] getColor() {
+		return color;
+	}
+	public void setColor(char[] color) {
+		this.color = color;
+	}
+	public char[] getBalance() {
+		return balance;
+	}
+	public void setBalance(char[] balance) {
+		this.balance = balance;
+	}
+	public char[] getFill() {
+		return fill;
+	}
+	public void setFill(char[] fill) {
+		this.fill = fill;
+	}
+	
+	
+	
 	
 }
+
+
--- Project0/src/readWrite/Main.java
+++ Project0/src/readWrite/Main.java
@@ -3,61 +3,101 @@ package readWrite;
 import java.util.Scanner;
 import java.io.*;
 
-
 public class Main {
 
-	private static Record[] records = new Record[10];
+	private static final int SIZE = 10;
+	private static Record[] records = new Record[SIZE];
+	private static FixedRecord[] fixedRecords = new FixedRecord[SIZE];
 	private static File file = new File("files.txt");
-	
-	
-	public static void main(String[] args) throws IOException{
+	private static int count = -1; // This counts how many records have been written. This is to prevent null
+									// pointers.
+
+	public static void main(String[] args) throws IOException {
 		int choice;
 		Scanner input = new Scanner(System.in);
-		
+
 		readRecord();
-		System.out.println("Enter a record number(0-4) you would like to view. Enter -1 to exit.");
-//		choice = input.nextInt();
-		
-		while((choice = input.nextInt()) != -1){
+		fillFixedRecords();
+
+		System.out.println(fixedRecords[0].getFirstname());
+		System.out.println(fixedRecords[1].getFirstname());
+		System.out.println(fixedRecords[2].getFirstname());
+		System.out.println(fixedRecords[3].getFirstname());
+		System.out.println(fixedRecords[4].getFirstname());
+
+		System.out.println("Enter a record number(0-4) you would like to view. Enter a number less than 0 to exit.");
+
+		while ((choice = input.nextInt()) > -1) {
 			System.out.println(records[choice]);
-			
-			System.out.println("Enter a record number(0-4) you would like to view. Enter -1 to exit.");
+
+			System.out.println("Enter a record number(0-4) you would like to view. Enter a number less than 0 to exit.");
 		}
 		System.out.println("Bye bye");
 
 	}
-	
-	
-	public static void readRecord() throws IOException{
 
-		BufferedReader buffer = new BufferedReader(new FileReader(file)); //wrappnig the filereader in a smarter file reader
+	public static void readRecord() throws IOException {
+
+		BufferedReader buffer = new BufferedReader(new FileReader(file)); // wrappnig the filereader in a smarter file
+																			// reader so I can read a whole line at a
+																			// time
 
 		Record r; // initialize variables needed inside loop
-		String s; 
-		int i = 0; //index for storing the record data to Record[] records field.
+		String s; // This will start as the whole line and will be chopped up from the front,
+					// seperated by commas
+		String m; // This will be the word that is chopped out.
+		int i = 0; // index for storing the record data to Record[] records field.
 
-		while( (s = buffer.readLine()) != null){
+		while ((s = buffer.readLine()) != null) {
+			count++; // If no records are read, count will remain -1.
 			r = new Record(); // Point to new Record object for each new record made.
 
 			r.setFirstname(s.substring(0, s.indexOf(',')));
-			s = s.substring(s.indexOf(',')+2, s.length());
+			s = s.substring(s.indexOf(',') + 2, s.length());
 			r.setLastname(s.substring(0, s.indexOf(',')));
-			s = s.substring(s.indexOf(',')+2, s.length());
+			s = s.substring(s.indexOf(',') + 2, s.length());
 			r.setEmail(s.substring(0, s.indexOf(',')));
-			s = s.substring(s.indexOf(',')+2, s.length());
+			s = s.substring(s.indexOf(',') + 2, s.length());
 			r.setIdnumber(Integer.parseInt(s.substring(0, s.indexOf(','))));
-			s = s.substring(s.indexOf(',')+2, s.length());
+			s = s.substring(s.indexOf(',') + 2, s.length());
 			r.setColor(s.substring(0, s.indexOf(',')));
-			s = s.substring(s.indexOf(',')+2, s.length());
+			s = s.substring(s.indexOf(',') + 2, s.length());
 			r.setBalance(Double.parseDouble(s.substring(0, s.length())));
-
 
 			records[i++] = r;
 		}
-		
-		buffer.close(); //closing buffer stream and releasing resources
-		
+
+		buffer.close(); // closing buffer stream and releasing resources
+
 	}
-	
-	
+
+	public static void fillFixedRecords() {
+
+		for (int i = 0; i < count + 1; i++) {// count is +1 to offset its initial state of -1.
+
+			fixedRecords[i] = new FixedRecord();
+
+			fixedRecords[i].setFirstname(fillSpaces(records[i].getFirstname(), 10));
+			fixedRecords[i].setLastname(fillSpaces(records[i].getLastname(), 15));
+			fixedRecords[i].setEmail(fillSpaces(records[i].getEmail(), 25));
+			fixedRecords[i].setIdnumber(fillSpaces(records[i].getIdnumber() + "", 10));
+			fixedRecords[i].setColor(fillSpaces(records[i].getColor(), 10));
+			fixedRecords[i].setBalance(fillSpaces(records[i].getBalance() + "", 10));
+			// fixedRecords[i].setFill(fill);
+		}
+
+	}
+
+	public static char[] fillSpaces(String s, int max) {
+		char[] spaces = "***************************".toCharArray();
+		char[] temp = s.toCharArray();
+		char[] c = new char[max];
+		System.arraycopy(temp, 0, c, 0, s.length());
+		if (s.length() < max) {
+			System.arraycopy(spaces, 0, c, s.length(), max - s.length());
+		}
+		return c;
+
+	}
+
 }
